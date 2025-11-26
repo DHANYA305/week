42f408028e285b348e5cc9d2d12d9e913aacc941
DHANYA305 <dhanyabhandari30@gmail.com>
2025-11-26 06:52:56 +0530

    Add Demo.java with a simple Java program

---
+++ Demo.java
@@ -0,0 +1,6 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java file (Demo.java)!");
+        System.out.println("Jenkins successfully executed Java program.");
+    }
+}
